4ff357fac4b2e336766cadefc023170aabba6af2
Gorgsenegger <tim@gorgs.org>
2026-06-12 11:21:07 +0200

    Fix

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,12 +1,9 @@
-// See: https://rollupjs.org/introduction/
-
 import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 
 const config = {
   input: 'src/index.ts',
-  external: ['@actions/core'],
   output: {
     esModule: true,
     file: 'dist/index.js',
